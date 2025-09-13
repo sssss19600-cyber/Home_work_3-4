59f5a19c6901663c87bbac68df1d9bff1c261d97
GameMoon <sssss19600@gmail.com>
2025-09-13 18:29:47 +0300

    Program find factorial of any number

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+factorial = 1
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    factorial *= i
+
+print(f"The factorial of {n} is {factorial}")
+
+#Примітивна программа, але робоча
