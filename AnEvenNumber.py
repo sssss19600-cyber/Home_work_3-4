a681dbd236f02595d972766752b3c470169665be
GameMoon <sssss19600@gmail.com>
2025-09-13 17:33:08 +0300

    Program writes an even numbers

---
+++ AnEvenNumber.py
@@ -0,0 +1,7 @@
+a = int(input("Enter an even number: "))
+
+for i in range(a, 0, -1):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+#Знову проста программа, не знаю що креативити тому такий короткий код
