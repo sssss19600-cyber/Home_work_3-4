4ff1c2c0805e1fb3c5547b8145ccea7e9258b619
GameMoon <sssss19600@gmail.com>
2025-09-13 18:40:02 +0300

    What is your grade?

---
+++ Grade.py
@@ -0,0 +1,12 @@
+grade = input("Enter a grade: ")
+
+if grade > str(89):
+    print("Perfect!")
+elif grade > str(69):
+    print("Excellent!")
+elif grade > str(49):
+    print("Good!")
+else:
+    print("Bad :(")
+
+#І знову, премитивно, але працює!
