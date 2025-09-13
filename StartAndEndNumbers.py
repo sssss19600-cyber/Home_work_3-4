783405618a65492f17e9f197500c362087ada4fe
GameMoon <sssss19600@gmail.com>
2025-09-13 17:15:21 +0300

    You pick a start number And end number Then program will counting numbers between you choose!

---
+++ StartAndEndNumbers.py
@@ -0,0 +1,7 @@
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+for i in range(start, end + 1):
+    print(i)
+
+#Не знаю що ще можно зробити креатівнише з цим кодом, тому просто программа яка виконує задачу
